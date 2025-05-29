24f1c7d9b31fa27a19b6f52986278d30d39f9017
Josua Kehlenbach <dev@josuakehlenbach.eu>
2025-05-29 10:37:28 +0200

    fix flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Build a cargo project without extra checks";
+  description = "Build a cargo workspace";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -7,46 +7,206 @@
     crane.url = "github:ipetkov/crane";
 
     flake-utils.url = "github:numtide/flake-utils";
+
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, crane, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      crane,
+      flake-utils,
+      advisory-db,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
+        inherit (pkgs) lib;
+
         craneLib = crane.mkLib pkgs;
+        src = craneLib.cleanCargoSource ./.;
 
         # Common arguments can be set here to avoid repeating them later
-        # Note: changes here will rebuild all dependency crates
         commonArgs = {
-          src = craneLib.cleanCargoSource ./.;
+          inherit src;
           strictDeps = true;
 
-          buildInputs = [
-            # Add additional build inputs here
-          ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-            # Additional darwin specific inputs can be set here
-            pkgs.libiconv
-          ];
+          buildInputs =
+            [
+              # Add additional build inputs here
+            ]
+            ++ lib.optionals pkgs.stdenv.isDarwin [
+              # Additional darwin specific inputs can be set here
+              pkgs.libiconv
+            ];
+
+          # Additional environment variables can be set directly
+          # MY_CUSTOM_VAR = "some value";
         };
 
-        rpg-combat-tui = craneLib.buildPackage (commonArgs // {
-          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        # Build *just* the cargo dependencies (of the entire workspace),
+        # so we can reuse all of that work (e.g. via cachix) when running in CI
+        # It is *highly* recommended to use something like cargo-hakari to avoid
+        # cache misses when building individual top-level-crates
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 
-          # Additional environment variables or build phases/hooks can be set
-          # here *without* rebuilding all dependency crates
-          # MY_CUSTOM_VAR = "some value";
-        });
+        individualCrateArgs = commonArgs // {
+          inherit cargoArtifacts;
+          # inherit (craneLib.crateNameFromCargoToml { inherit src; }) version;
+          # NB: we disable tests since we'll run them all via cargo-nextest
+          doCheck = false;
+        };
+
+        fileSetForCrate =
+          crate:
+          lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./Cargo.toml
+              ./Cargo.lock
+              (craneLib.fileset.commonCargoSources ./rpg-combat-tui)
+              (craneLib.fileset.commonCargoSources ./player-client)
+              (craneLib.fileset.commonCargoSources ./shared)
+              (craneLib.fileset.commonCargoSources crate)
+            ];
+          };
+
+        # Build the top-level crates of the workspace as individual derivations.
+        # This allows consumers to only depend on (and build) only what they need.
+        # Though it is possible to build the entire workspace as a single derivation,
+        # so this is left up to you on how to organize things
+        #
+        # Note that the cargo workspace must define `workspace.members` using wildcards,
+        # otherwise, omitting a crate (like we do below) will result in errors since
+        # cargo won't be able to find the sources for all members.
+        rpg-combat-tui = craneLib.buildPackage (
+          individualCrateArgs
+          // {
+            pname = "rpg-combat-tui";
+            version = "0.1.0";
+            # version = (craneLib.crateNameFromCargoToml { cargoToml = ./rpg-combat-tui/Cargo.toml; }).version;
+            cargoExtraArgs = "-p rpg-combat-tui";
+            src = fileSetForCrate ./rpg-combat-tui;
+          }
+        );
+        player-client = craneLib.buildPackage (
+          individualCrateArgs
+          // {
+            pname = "player-client";
+            version = "0.1.0";
+            # version = (craneLib.crateNameFromCargoToml { cargoToml = ./player-client/Cargo.toml; }).version;
+            cargoExtraArgs = "-p player-client";
+            src = fileSetForCrate ./player-client;
+          }
+        );
+        shared = craneLib.buildPackage (
+          individualCrateArgs
+          // {
+            pname = "shared";
+            version = "0.1.0";
+            # version = (craneLib.crateNameFromCargoToml { cargoToml = ./shared/Cargo.toml; }).version;
+            cargoExtraArgs = "-p shared";
+            src = fileSetForCrate ./shared;
+          }
+        );
       in
       {
         checks = {
-          inherit rpg-combat-tui;
+          # Build the crates as part of `nix flake check` for convenience
+          inherit rpg-combat-tui player-client shared;
+
+          # Run clippy (and deny all warnings) on the workspace source,
+          # again, reusing the dependency artifacts from above.
+          #
+          # Note that this is done as a separate derivation so that
+          # we can block the CI if there are issues here, but not
+          # prevent downstream consumers from building our crate by itself.
+          my-workspace-clippy = craneLib.cargoClippy (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+            }
+          );
+
+          my-workspace-doc = craneLib.cargoDoc (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+            }
+          );
+
+          # Check formatting
+          my-workspace-fmt = craneLib.cargoFmt {
+            inherit src;
+          };
+
+          my-workspace-toml-fmt = craneLib.taploFmt {
+            src = pkgs.lib.sources.sourceFilesBySuffices src [ ".toml" ];
+            # taplo arguments can be further customized below as needed
+            # taploExtraArgs = "--config ./taplo.toml";
+          };
+
+          # Audit dependencies
+          my-workspace-audit = craneLib.cargoAudit {
+            inherit src advisory-db;
+          };
+
+          # Audit licenses
+          my-workspace-deny = craneLib.cargoDeny {
+            inherit src;
+          };
+
+          # Run tests with cargo-nextest
+          # Consider setting `doCheck = false` on other crate derivations
+          # if you do not want the tests to run twice
+          my-workspace-nextest = craneLib.cargoNextest (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              partitions = 1;
+              partitionType = "count";
+              cargoNextestPartitionsExtraArgs = "--no-tests=pass";
+            }
+          );
+
+          # Ensure that cargo-hakari is up to date
+          my-workspace-hakari = craneLib.mkCargoDerivation {
+            inherit src;
+            pname = "my-workspace-hakari";
+            cargoArtifacts = null;
+            doInstallCargoArtifacts = false;
+
+            buildPhaseCargoCommand = ''
+              cargo hakari generate --diff  # workspace-hack Cargo.toml is up-to-date
+              cargo hakari manage-deps --dry-run  # all workspace crates depend on workspace-hack
+              cargo hakari verify
+            '';
+
+            nativeBuildInputs = [
+              pkgs.cargo-hakari
+            ];
+          };
         };
 
-        packages.default = rpg-combat-tui;
+        packages = {
+          inherit rpg-combat-tui player-client shared;
+        };
 
-        apps.default = flake-utils.lib.mkApp {
-          drv = rpg-combat-tui;
+        apps = {
+          rpg-combat-tui = flake-utils.lib.mkApp {
+            drv = rpg-combat-tui;
+          };
+          player-client = flake-utils.lib.mkApp {
+            drv = player-client;
+          };
         };
 
         devShells.default = craneLib.devShell {
@@ -58,9 +218,9 @@
 
           # Extra inputs can be added here; cargo and rustc are provided by default.
           packages = [
-            # pkgs.ripgrep
-            pkgs.rust-analyzer
+            pkgs.cargo-hakari
           ];
         };
-      });
+      }
+    );
 }
