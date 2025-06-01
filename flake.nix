0ea49f6ca5ca13594f0f3ed3b536c44adf2e0325
Josua Kehlenbach <dev@josuakehlenbach.eu>
2025-06-01 13:44:12 +0200

    flake.nis

--- flake.nix
+++ flake.nix
@@ -27,40 +27,31 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-
         inherit (pkgs) lib;
 
         craneLib = crane.mkLib pkgs;
-        src = craneLib.cleanCargoSource ./.;
 
-        # Common arguments can be set here to avoid repeating them later
+        # Use workspace root directly, since it is a proper cargo workspace
+        src = ./.;
+
         commonArgs = {
-          inherit src;
+          src = ./.;
+          pname = "my-workspace";
+          version = "0.0.0";
           strictDeps = true;
 
           buildInputs =
             [
               # Add additional build inputs here
-            ]
-            ++ lib.optionals pkgs.stdenv.isDarwin [
-              # Additional darwin specific inputs can be set here
+            ] ++ lib.optionals pkgs.stdenv.isDarwin [
               pkgs.libiconv
             ];
-
-          # Additional environment variables can be set directly
-          # MY_CUSTOM_VAR = "some value";
         };
 
-        # Build *just* the cargo dependencies (of the entire workspace),
-        # so we can reuse all of that work (e.g. via cachix) when running in CI
-        # It is *highly* recommended to use something like cargo-hakari to avoid
-        # cache misses when building individual top-level-crates
         cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 
         individualCrateArgs = commonArgs // {
           inherit cargoArtifacts;
-          # inherit (craneLib.crateNameFromCargoToml { inherit src; }) version;
-          # NB: we disable tests since we'll run them all via cargo-nextest
           doCheck = false;
         };
 
@@ -74,44 +65,32 @@
               (craneLib.fileset.commonCargoSources ./rpg-combat-tui)
               (craneLib.fileset.commonCargoSources ./player-client)
               (craneLib.fileset.commonCargoSources ./shared)
-              (craneLib.fileset.commonCargoSources crate)
+              # (craneLib.fileset.commonCargoSources crate)
             ];
           };
 
-        # Build the top-level crates of the workspace as individual derivations.
-        # This allows consumers to only depend on (and build) only what they need.
-        # Though it is possible to build the entire workspace as a single derivation,
-        # so this is left up to you on how to organize things
-        #
-        # Note that the cargo workspace must define `workspace.members` using wildcards,
-        # otherwise, omitting a crate (like we do below) will result in errors since
-        # cargo won't be able to find the sources for all members.
         rpg-combat-tui = craneLib.buildPackage (
-          individualCrateArgs
-          // {
+          individualCrateArgs // {
             pname = "rpg-combat-tui";
             version = "0.1.0";
-            # version = (craneLib.crateNameFromCargoToml { cargoToml = ./rpg-combat-tui/Cargo.toml; }).version;
             cargoExtraArgs = "-p rpg-combat-tui";
             src = fileSetForCrate ./rpg-combat-tui;
           }
         );
+
         player-client = craneLib.buildPackage (
-          individualCrateArgs
-          // {
+          individualCrateArgs // {
             pname = "player-client";
             version = "0.1.0";
-            # version = (craneLib.crateNameFromCargoToml { cargoToml = ./player-client/Cargo.toml; }).version;
             cargoExtraArgs = "-p player-client";
             src = fileSetForCrate ./player-client;
           }
         );
+
         shared = craneLib.buildPackage (
-          individualCrateArgs
-          // {
+          individualCrateArgs // {
             pname = "shared";
             version = "0.1.0";
-            # version = (craneLib.crateNameFromCargoToml { cargoToml = ./shared/Cargo.toml; }).version;
             cargoExtraArgs = "-p shared";
             src = fileSetForCrate ./shared;
           }
@@ -224,3 +203,4 @@
       }
     );
 }
+
